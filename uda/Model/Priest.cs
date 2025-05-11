fce4010d5ddf43b9cfaa614fe31c11fd5332ab84
PanHands <Swordsmith.nh@gmail.com>
2025-05-10 21:24:19 -0700

    Added a monster factory that pulls from a db

--- uda/Model/Priest.cs
+++ uda/Model/Priest.cs
@@ -2,18 +2,18 @@ namespace UDA.Model;
 
 public class Priest : Hero
 {
-    private static int _myHitPoints = 75;
+    private static int myHitPoints = 75;
     private static readonly int MyAttackSpeed = 5;
     private static readonly double MyHitChance = 0.7;
     private static readonly (int, int) MyDamageRange = (25, 45);
     private static readonly double MyBlockChance = 0.3;
     private static readonly string MySkill = "Heal";
     
-    public Priest(string theName) : base(in theName, in _myHitPoints, in MyAttackSpeed, in MyHitChance,
-        in MyDamageRange, in MyBlockChance, in MySkill) { }
+    public Priest(string theName) : base(theName, myHitPoints,MyAttackSpeed, MyHitChance,
+        MyDamageRange, MyBlockChance, MySkill) { }
 
     public void Heal()
     {
-        _myHitPoints += 20;
+        myHitPoints += 20;
     }
 }

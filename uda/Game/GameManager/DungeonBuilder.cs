3ec5c6187fc17afd0ff08ef756a16087d073a431
PanHands <Swordsmith.nh@gmail.com>
2025-06-01 12:44:33 -0700

    Added builder that creates dungeon from rooms. Created resource of different room types.

---
+++ uda/Game/GameManager/DungeonBuilder.cs
@@ -0,0 +1,47 @@
+using Godot;
+using System;
+using Godot.Collections;
+using UDA.Model.Map;
+
+public partial class DungeonBuilder : Node2D
+{
+	private static RoomTypeCollection _myRoomTypes = new RoomTypeCollection();
+	private static Dictionary<RoomTypeCollection.RoomType, PackedScene> _myRoomTypeDict = _myRoomTypes.RoomDictionary;
+	private const int MyRoomSideWidth = 224;
+	private const int MyRoomHeight = 174;
+	private int _myDungeonLength = 5;
+	private int _myDungeonWidth = 5;
+	
+	// Called when the node enters the scene tree for the first time.
+	// do not want this, only want to run if new game is called which should build the map.
+	public override void _Ready()
+	{
+		BuildDungeon();
+	}
+
+	//This would read through the values in the 2d array representing the dunegon
+	//and instantiate the add the corresponding room in the engine(game)
+	public void BuildDungeon()
+	{
+		for (int i = 0; i < _myDungeonLength; i++)
+		{
+			for (int j = 0; j < _myDungeonWidth; j++)
+			{
+				//Access the room type and figure out what relates to the appropriate key in the dict
+				//Probably a bunch of conditionals
+				//This is only going to load in a bunch of four door rooms
+				//TODO: Update the logic here to select the correct room type 
+				//This should almost certainly be placed into a helper method as it is essentially going to be a huge
+				//switch statement
+				PackedScene roomToLoad = _myRoomTypeDict[RoomTypeCollection.RoomType.FourDoor];
+				//Instantiate
+				//TODO: This should also probably be placed into helper method VVV
+				var instancedRoom = roomToLoad.Instantiate();
+				instancedRoom.Set(Node2D.PropertyName.GlobalPosition,
+					new Vector2((float)i * MyRoomSideWidth, (float)j * MyRoomHeight));
+				AddChild(instancedRoom);
+			}
+		}
+	}
+
+}
---
+++ uda/Game/Resources/RoomTypeCollection.cs
@@ -0,0 +1,45 @@
+using Godot;
+using Godot.Collections;
+[GlobalClass]
+public partial class RoomTypeCollection: Resource
+{
+	[Export]
+	public Dictionary<RoomType, PackedScene> RoomDictionary { get; set; } = new()
+	{
+		{ RoomType.OneDoorEast, ResourceLoader.Load<PackedScene>("res://Rooms/OneDoorEast.tscn")},
+		{ RoomType.OneDoorNorth, ResourceLoader.Load<PackedScene>("res://Rooms/OneDoorNorth.tscn")},
+		{ RoomType.OneDoorSouth, ResourceLoader.Load<PackedScene>("res://Rooms/OneDoorSouth.tscn")},
+		{ RoomType.OneDoorWest, ResourceLoader.Load<PackedScene>("res://Rooms/OneDoorWest.tscn")},
+		{ RoomType.TwoDoorEastWest, ResourceLoader.Load<PackedScene>("res://Rooms/TwoDoorEastWest.tscn")},
+		{ RoomType.TwoDoorNorthEast, ResourceLoader.Load<PackedScene>("res://Rooms/TwoDoorNorthEast.tscn")},
+		{ RoomType.TwoDoorNorthSouth, ResourceLoader.Load<PackedScene>("res://Rooms/TwoDoorNorthSouth.tscn")},
+		{ RoomType.TwoDoorNorthWest, ResourceLoader.Load<PackedScene>("res://Rooms/TwoDoorNorthWest.tscn")},
+		{ RoomType.TwoDoorSouthEast, ResourceLoader.Load<PackedScene>("res://Rooms/TwoDoorSouthEast.tscn")},
+		{ RoomType.TwoDoorSouthWest, ResourceLoader.Load<PackedScene>("res://Rooms/TwoDoorSouthWest.tscn")},
+		{ RoomType.ThreeDoorNorthEastWest, ResourceLoader.Load<PackedScene>("res://Rooms/ThreeDoorNorthEastWest.tscn")},
+		{ RoomType.ThreeDoorNorthSouthEast, ResourceLoader.Load<PackedScene>("res://Rooms/ThreeDoorNorthSouthEast.tscn")},
+		{ RoomType.ThreeDoorNorthSouthWest, ResourceLoader.Load<PackedScene>("res://Rooms/ThreeDoorNorthSouthWest.tscn")},
+		{ RoomType.ThreeDoorSouthEastWest, ResourceLoader.Load<PackedScene>("res://Rooms/ThreeDoorSouthEastWest.tscn")},
+		{ RoomType.FourDoor, ResourceLoader.Load<PackedScene>("res://Rooms/FourDoor.tscn")},
+	};
+
+	public enum RoomType
+	{
+		OneDoorEast,
+		OneDoorNorth,
+		OneDoorWest,
+		OneDoorSouth,
+		TwoDoorNorthSouth,
+		TwoDoorNorthWest,
+		TwoDoorNorthEast,
+		TwoDoorSouthWest,
+		TwoDoorSouthEast,
+		TwoDoorEastWest,
+		ThreeDoorNorthEastWest,
+		ThreeDoorNorthSouthEast,
+		ThreeDoorNorthSouthWest,
+		ThreeDoorSouthEastWest,
+		FourDoor
+	}
+
+}
